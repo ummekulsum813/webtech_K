ee4e398f996423aeb2e5c3018ac2fb80fced214d
Umme Kulsum <ummekulsum813@gmail.com>
2021-10-11 12:35:45 +0600

    Add files via upload

---
+++ Task 2/json.php
@@ -0,0 +1,10 @@
+[
+    {
+        "First name": "umme",
+        "Last name": "kulsum",
+        "Age": "2",
+        "Email": "ummekulsum813@gmail.com",
+        "Pasword": "222",
+        "Designation": "CSE"
+    }
+]
